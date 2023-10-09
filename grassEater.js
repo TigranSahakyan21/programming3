3d5b1459460e29afea07a841cdd725167f1bbd9c
TigranSahakyan21 <tigransahakyan5.y@tumo.org>
2023-10-09 17:36:50 +0400

    server em sarqe

--- grassEater.js
+++ grassEater.js
@@ -1,7 +1,6 @@
-class GrassEater {
+class GrassEater extends LivingCreature {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        super(x,y)
         this.energy = 8;
         this.directions = [];
     }
